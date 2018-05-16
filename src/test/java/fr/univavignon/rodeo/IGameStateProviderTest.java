0f08b2d4434325c718c2c0113661617cd268986b
faycal bouhara <faycal.bouhara@alumni.univ-avignon.fr>
2018-05-16 13:48:50 +0200

    All test

--- src/test/java/fr/univavignon/rodeo/IGameStateProviderTest.java
+++ src/test/java/fr/univavignon/rodeo/IGameStateProviderTest.java
@@ -1,5 +1,80 @@
 package fr.univavignon.rodeo;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doThrow;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import fr.univavignon.rodeo.api.IAnimal;
+import fr.univavignon.rodeo.api.IEnvironment;
+import fr.univavignon.rodeo.api.IEnvironmentProvider;
+import fr.univavignon.rodeo.api.IGameState;
+import fr.univavignon.rodeo.api.IGameStateProvider;
+import fr.univavignon.rodeo.api.ISpecie;
+import fr.univavignon.rodeo.api.SpecieLevel;
+
 public class IGameStateProviderTest {
+	
+	
+
+		
+		
+		static IGameStateProvider gameStateProvider;
+		
+		static IGameState gameState;
+		
+		static ISpecie specie;
+		
+		static ArrayList<ISpecie> listeSpices;	
+		
+		static IAnimal bimbo =new IAnimalTest().MockAnimal();
+			
+		
+		    public static  IGameStateProvider MockGameStateProvider(){
+		    	
+		    
+		        gameStateProvider=Mockito.mock(IGameStateProvider.class);		        
+		        Mockito.doThrow(new IllegalArgumentException()).when(gameStateProvider).get(null);
+		        
+		        gameState = IGameStateTest.MockGameState();			        
+		        Mockito.when(gameStateProvider.get("nothink")).thenReturn(gameState); 
+		        
+		       
+		        
+		        return gameStateProvider;
+		        
+		 }
+		    
+		    
+		    
+		    @Before  
+		    public void initialisation(){
+		    	gameStateProvider= MockGameStateProvider();
+		   	 
+		    }
+	
+		    
+		    
+		    @Test(expected=IllegalArgumentException.class)
+			public void testGetExcep(){
+				gameStateProvider.get(null);
+				
+		}
+	
+	
+		    @Test
+			public void testGet(){
+				
+				assertEquals(gameState, gameStateProvider.get("nothink"));
+				
+		}
+	
+		    
+	
+	
 
 }
